fada1cfc252d5b35d4b9d9b84a1907f04da2b556
Kevin <67619628+kev237@users.noreply.github.com>
2022-01-18 19:29:01 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * VoltageCurrent_PowerMeasuring_V2.c
+ *
+ * Created: 23.09.2020 07:22:59
+ * Author : Kevin Kamdem
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
